d31189240f8c9e3f1433948758fc3734bf346e98
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2026-01-26 22:51:38 -0300

    new: Added comment implementation code.

---
+++ fundamentals/comments.js
@@ -0,0 +1,12 @@
+//Comentario aqui
+console.log('Linha 1'); //Aqui será ignorado
+console.log('Linha 2'); //Aqui será ignorado
+console.log('Linha 3'); //Aqui será ignorado
+console.log('Linha 4'); //Aqui será ignorado
+
+/*
+Comentario também
+linha 1
+linha 2
+linha 3
+*/
