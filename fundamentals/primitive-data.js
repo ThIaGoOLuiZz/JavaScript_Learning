ca2665e4cc4e7192b8d9bf684340258e3ee110c5
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2026-01-27 18:48:22 -0300

    new: added new primitive datas script

---
+++ fundamentals/primitive-data.js
@@ -0,0 +1,22 @@
+//String
+
+const nome = 'Luiz';
+const nome1 = "Luiz";
+const nome2 = `Luiz`;
+
+// Number
+
+const num1 = 10;
+const num2 = 10.0;
+
+//Undefined
+
+let nomeAluno;
+
+//null
+
+let sobrenomeAluno = null;
+
+//Boolean
+
+const bool = true;
