f878b2184228e3b856e9dc85deb109bb0304944c
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2026-01-27 19:04:19 -0300

    new: added new arithmetic operation code

---
+++ fundamentals/arithmetic-operators.js
@@ -0,0 +1,16 @@
+const num1 = 2;
+const num2 = 10;
+console.log(num1 + num2);   //Soma
+console.log(num1 * num2);   //Multiplicação
+console.log(num1 / num2);   //Divisão
+console.log(num1 - num2);   //Subtração
+console.log(num1 ** num2);  //Potenciação
+console.log(num1 % num2);   //Resto da divisão
+
+//Conversão de valores
+const num3 = parseInt('5')
+const num4 = Number('5')
+
+let contador = 1;
+contador++;
+contador+=2;
