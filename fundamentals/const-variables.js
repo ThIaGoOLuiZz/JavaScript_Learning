a86fe9f47718020b66b898e7acdec3108cb71152
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2026-01-26 23:21:55 -0300

    new: Implemented tests on constant variables.

---
+++ fundamentals/const-variables.js
@@ -0,0 +1,11 @@
+const nome = 'João';
+console.log(nome);
+
+// nome = 'Thiago' -> Não pode ser alterado
+
+const primeiroNum = 5;
+const segundoNum  = 10;
+const result = primeiroNum * segundoNum;
+
+console.log(result);
+console.log(typeof(primeiroNum)); // Obter o tipo
