150020fc1fc51fd0cde8eb3d2b6943eb3cc644db
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2026-01-29 22:05:52 -0300

    new: adding new number variables script

---
+++ fundamentals/number-variables.js
@@ -0,0 +1,6 @@
+let num1 = 1;
+let num2 = 2.52222;
+console.log(num1.toString() + num2.toString());
+console.log(num2.toFixed(2));
+console.log(Number.isInteger(num1));
+console.log(Number.isNaN(num1));
