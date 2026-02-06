e25b2dd96623f9e82591b0157d4628903ffe4e5e
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2026-02-06 18:14:04 -0300

    new: adding new ternary script

---
+++ fundamentals/ternary operator.js
@@ -0,0 +1,9 @@
+const pontuacao = 999;
+
+const nivel = pontuacao >= 100 ? true : false;
+console.log(nivel);
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'Preta';
+
+console.log(corPadrao)
